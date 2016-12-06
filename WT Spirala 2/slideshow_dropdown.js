727db502a4df2cd2ff59010eebffc508264ce0eb
JasminkoVidakovic <darkvalkirion@gmail.com>
2016-12-06 21:40:19 +0100

    Update slideshow_dropdown.js

--- WT Spirala 2/slideshow_dropdown.js
+++ WT Spirala 2/slideshow_dropdown.js
@@ -2,33 +2,25 @@ var timeout	= 1000;
 var closetimer	= 0;
 var ddmenuitem	= 0;
 
-// open hidden layer
 function mopen(id)
 {	
-	// cancel close timer
 	mcancelclosetime();
-
-	// close old layer
 	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
 
-	// get new layer and show it
 	ddmenuitem = document.getElementById(id);
 	ddmenuitem.style.visibility = 'visible';
-
 }
-// close showed layer
+
 function mclose()
 {
 	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
 }
 
-// go close timer
 function mclosetime()
 {
 	closetimer = window.setTimeout(mclose, timeout);
 }
 
-// cancel close timer
 function mcancelclosetime()
 {
 	if(closetimer)
@@ -38,5 +30,4 @@ function mcancelclosetime()
 	}
 }
 
-// close layer when click-out
 document.onclick = mclose; 
